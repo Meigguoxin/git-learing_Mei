5d17c700b95d1438292ab8c03d49b8322fd3c678
Meigguoxin <459382438@qq.com>
2024-05-31 00:19:32 +0800

    feat: 新功能

--- commlint.config.js
+++ commlint.config.js
@@ -1,30 +1,27 @@
-const types = [
-    'fix',
-    'feat',
-    'bug'
-  ];
-  
-  typeEnum={
-    rules:{
-      'type-enum': [2, 'always', types]
+module.exports = {
+    // 继承的规则
+    extends: ['@commitlint/config-conventional'],
+    // 定义规则类型
+    rules: {
+      // type 类型定义，表示 git 提交的 type 必须在以下类型范围内
+      'type-enum': [
+        2,
+        'always',
+        [
+          'feat', // 新功能 feature
+          'fix', // 修复 bug
+          'docs', // 文档注释
+          'style', // 代码格式(不影响代码运行的变动)
+          'refactor', // 重构(既不增加新功能，也不是修复bug)
+          'perf', // 性能优化
+          'test', // 增加测试
+          'chore', // 构建过程或辅助工具的变动
+          'revert', // 回退
+          'build', // 打包
+          'ci', // 与持续集成服务有关的改动
+        ],
+      ],
+      // subject 大小写不做校验
+      'subject-case': [0],
     },
-    value:() => types
   }
-  
-  module.exports = {
-    extends: [
-      "@commitlint/config-conventional"
-    ],
-      rules: {
-          'body-leading-blank': [1, 'always'],
-          'footer-leading-blank': [1, 'always'],
-          'header-max-length': [2, 'always', 72],
-          'scope-case': [2, 'always', 'sentence-case'],
-          'subject-case': [2,'always','sentence-case'],
-          'subject-empty': [2, 'never'],
-          'subject-full-stop': [2, 'never', '.'],
-          'type-case': [2, 'always', 'lower-case'],
-          'type-empty': [2, 'never'],
-          'type-enum': typeEnum.rules['type-enum']
-      }
-  };
